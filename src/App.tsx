cc0d085990bcb2b1dd1590beb74776ee865798d7
Luis Fernando <luisfernando.mourag@gmail.com>
2025-09-01 20:51:16 -0300

    Finalizando componente Heading e Tipagem TypeScript de children

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { TimerIcon } from 'lucide-react';
 import { Heading } from './components/Heading';
 
 import './styles/global.css'
@@ -5,7 +6,12 @@ import './styles/theme.css'
 export function App() {
     return (
         <>
-            <Heading />
+            <Heading>
+                hello
+                <button>
+                    <TimerIcon />
+                </button>
+            </Heading>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quaerat a sapiente doloremque non nisi, facere corporis possimus est tempora error enim quasi similique laudantium omnis pariatur aliquam commodi libero.</p>
         </>
     )
