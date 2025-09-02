f371abd5f386bada7dc9ebbcee4ae364eef7d21b
Luis Fernando <luisfernando.mourag@gmail.com>
2025-09-02 08:28:45 -0300

    Criando estrutura HTML e CSS do app

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
-import { TimerIcon } from 'lucide-react';
-import { Heading } from './components/Heading';
 
 import './styles/global.css'
 import './styles/theme.css'
 export function App() {
     return (
         <>
-            <Heading>
-                hello
-                <button>
-                    <TimerIcon />
-                </button>
-            </Heading>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quaerat a sapiente doloremque non nisi, facere corporis possimus est tempora error enim quasi similique laudantium omnis pariatur aliquam commodi libero.</p>
+            <div className="container">
+                <div className="content">
+                    <section>
+                        <span>Logo</span>
+                    </section>
+                </div>
+            </div>
+
+            <div className='container'>
+                <div className='content'>
+                    <section>MENU</section>
+                </div>
+            </div>
+
         </>
     )
 }
